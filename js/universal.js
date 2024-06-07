78e87bdfe07ff5c3a75bedde8512d8e2feb77aa3
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-06-07 18:49:11 -0300

    [refactor] Movendo conteúdo para arquivo certo
    Conteúdo de estilização do banner estava na estilização dinamica do universal (o qual não o é), então foi necessário mover do arquivo universal.js para index.js.

--- js/universal.js
+++ js/universal.js
@@ -1,13 +1,5 @@
 /*-----===| VARIÁVEIS |===-----*/
-var headerHeight;
-var bannerTextContainer = document.getElementsByClassName('banner-text-container')[0];
 
 /*-----===| EVENTOS |===-----*/
-window.onload = calcBannerTextHeight;
-window.addEventListener('resize', calcBannerTextHeight);
 
 /*-----===| FUNÇÕES |===-----*/
-function calcBannerTextHeight() {
-    headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
-    bannerTextContainer.style.height = "calc(100% - " + headerHeight + "px)";
-}
